c08f76f1d82aa0f7327f3fc08a59073c55bb3a14
tikhonov <uni.nova.tih@gmail.com>
2020-03-10 00:13:51 +0100

    open_lock

--- Algorithms & Data Structes/LeetCode/Queue & Stack/Open the Lock.playground/Contents.swift
+++ Algorithms & Data Structes/LeetCode/Queue & Stack/Open the Lock.playground/Contents.swift
@@ -9,7 +9,51 @@ import UIKit
 // Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
 
 func openLock(_ deadends: [String], _ target: String) -> Int {
+    var cache: Set<[Int]> = []
+    var queue: [[Int]] = []
     
+    let current = [0, 0, 0, 0]
+    let deads: Set<[Int]> = Set(deadends.map { $0.map { Int(String($0))! } })
+    let target: [Int] = target.map { Int(String($0))! }
+    var counter = 0
+    queue.append(current)
+    cache.insert(current)
+    guard !deads.contains(target) else { return -1 }
+    var temp = [Int]()
+    while !queue.isEmpty {
+        for _ in 0..<queue.count {
+            
+            let str = queue.removeFirst()
+            
+            if deads.contains(str) {
+                continue
+            }
+            
+            if str == target {
+                return counter
+            }
+            
+            for i in 0...3 {
+                for d in stride(from: -1, through: 1, by: 2) {
+                    temp = str
+                    temp[i] = (10 + temp[i] + d) % 10
+                    if !deads.contains(temp) && !cache.contains(temp) {
+                        queue.append(temp)
+                        cache.insert(temp)
+                    }
+                }
+            }
+            
+        }
+        counter += 1
+    }
+    
+    return -1
 }
 
+
+openLock(["0201","0101","0102","1212","2002"], "0202")
+openLock(["8888"], "0009")
+openLock(["8887","8889","8878","8898","8788","8988","7888","9888"], "8888")
+openLock(["0000"], "8888")
 
