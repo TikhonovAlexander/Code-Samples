36ef2c907adbcef5e27f54071185834aab3ae174
atikhonov <uni.nova.tih@gmail.com>
2020-04-12 16:58:12 +0200

    lastStoneWeight

---
+++ Algorithms & Data Structes/LeetCode/30day challenge/Last Stone Weight.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// We have a collection of stones, each stone has a positive integer weight.
+
+// Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+// If x == y, both stones are totally destroyed;
+// If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+// At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+func lastStoneWeight(_ stones: [Int]) -> Int {
+    
+    var stones = stones.sorted()
+    
+    while stones.count > 1 {
+        print(stones)
+        let size = stones.count - 1
+        if stones[size] == stones[size - 1] {
+            stones = stones.dropLast(2)
+        } else {
+            stones[size - 1] = stones[size] - stones[size - 1]
+            stones = stones.dropLast()
+            stones = stones.sorted()
+        }
+    }
+    
+    return stones.first ?? 0
+}
+
+lastStoneWeight([2,7,4,1,8,1])
