b2adb9d67c54d701c8924ea8c4fb4f3bbf00e79e
atikhonov <uni.nova.tih@gmail.com>
2020-04-26 13:14:44 +0200

    max jump

---
+++ Algorithms & Data Structes/LeetCode/30day challenge/Jump Game.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+// Each element in the array represents your maximum jump length at that position.
+
+// Determine if you are able to reach the last index.
+
+func canJump(_ nums: [Int]) -> Bool {
+    
+    var maxJump = nums[0]
+    
+    for i in 0..<nums.count {
+        
+        if i > maxJump {
+            return false
+        }
+        
+        maxJump = max(maxJump, i + nums[i])
+    }
+    
+    return true
+}
+
+
+
+// canJump([1, 0])
+// canJump([3,1,0,0,0,34])
+canJump( [6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1, 2, 6, 5, 3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1, 2, 0, 1, 4, 6, 5, 8, 0, 7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0, 9, 8, 4, 3, 0, 7, 7, 1, 9, 1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6])
+//canJump([1,2,3,4,5,6,7,8])
